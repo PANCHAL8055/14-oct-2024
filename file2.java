7380710c3900f301fa7a7a7af1fe018ab7bb2f6e
Vikas <106225419+PANCHAL8055@users.noreply.github.com>
2024-10-14 14:55:12 +0530

    Add files via upload

---
+++ file2.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class file2 {
+    public static void main(String[] args) {
+
+        char a = 'G';
+        int i = 89;
+        byte b = 4;
+        short s = 56;
+       
+        double d = 4.35542;
+        float f = 4.7334f;
+        long l = 12121;
+
+        System.out.println(a);
+        System.out.println(i);
+        System.out.println(b);
+        System.out.println(s);
+        System.out.println(f);
+        System.out.println( d);
+        System.out.println(l);
+    
+        
+    }
+    
+}
